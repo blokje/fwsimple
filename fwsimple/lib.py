e5037291a97f1361c00cb2cb508991e6c478bf0f
Rick Voormolen <rick@voormolen.org>
2021-03-30 16:23:37 +0200

    Remove unreachable code

--- fwsimple/lib.py
+++ fwsimple/lib.py
@@ -7,16 +7,7 @@ import importlib
 class FirewallExecution(object):
     def __str__(self):
         """ Return formatted string based on execution type """
-        args = None
         return repr(self)
-        if self._firewall.exec_type == constants.EXEC_IPTABLES:
-            args = self.args_iptables()
-
-        if not args:
-            return repr(self)
-        else:
-            for expression in args:
-                return " ".join([str(argument) for argument in expression])
 
     def args_iptables(self):
         raise NotImplementedError("This function is not (yet) implemented")
